7cb1b83a967b8d1ddf2999b47f1b2cf5b6755d3b
Vladimir Mikryukov <v.mikryukov@gmail.com>
2018-10-12 16:46:26 +0500

    add drop_NA_col

--- R/drop.R
+++ R/drop.R
@@ -2,3 +2,9 @@
 drop_mono <- function(x){
   x[, apply(x, 2, function(x) length(unique(x)) > 1)]
 }
+
+drop_NA_col <- function(df){
+    colz <- apply(is.na(df), 2, all)
+    res <- df[ , !colz ]
+    return(res)
+}
