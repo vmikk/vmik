69ab8f9390bc72ab91f521f805d77f200ffcc4b0
Vladimir Mikryukov <v.mikryukov@gmail.com>
2018-10-17 13:53:03 +0500

    move gelman_scale to transformations

--- R/transformations.R
+++ R/transformations.R
@@ -1,4 +1,14 @@
 
+gelman_scale <- function(x){
+  x.obs <- x[!is.na(x)]
+  mm <- mean(x.obs)
+  ss <- sd(x.obs)
+  res <- (x - mm)/(2 * ss)
+  attr(res, "scaled:center") <- mm
+  attr(res, "scaled:scale") <- ss
+  return(res)
+}
+
 IHS <- function(x){ 
   log(x + sqrt(x^2 + 1))
 }
