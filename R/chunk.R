7a66117effe5a7ed89b5217df727a83ebc252c5f
Vladimir Mikryukov <v.mikryukov@gmail.com>
2018-10-23 11:17:15 +0500

    update chunk

--- R/chunk.R
+++ R/chunk.R
@@ -1,7 +1,9 @@
 
 # Split a vector into chunks equal size
 # x = vector; n = number of chunks
-chunk <- function(x, n) split(x, cut(seq_along(x), n, labels = FALSE))
+chunk <- function(x, n){
+  split(x, cut(seq_along(x), n, labels = FALSE))
+}
 
 ## for integer vector
 # n = 3
