40520ff6003b0936c6565b9312a227bead2f0a40
Vladimir Mikryukov <v.mikryukov@gmail.com>
2018-10-24 17:08:51 +0500

    roxygenize drop_mono

--- R/drop.R
+++ R/drop.R
@@ -1,8 +1,17 @@
 
-# Erase monomorphic columns from data
+#' Erase monomorphic columns from data
+#'
+#' @description Removes not varying variables (columns with same values) from data.
+#' @param x A data.frame or matrix.
+#' @return data.frame or matrix without monomorphic columns.
+#' @examples
+#' datt <- data.frame(V1=1:50, V2=rnorm(50), V3=rep(7, 50), V4=rnorm(50))
+#' drop_mono(datt)
+#' 
 drop_mono <- function(x){
   x[, apply(x, 2, function(x) length(unique(x)) > 1)]
 }
+
 
 drop_NA_col <- function(df){
     colz <- apply(is.na(df), 2, all)
