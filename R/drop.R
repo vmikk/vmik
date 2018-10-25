74224e8acfb5c320f5bec5523896c8374056f937
Vladimir Mikryukov <v.mikryukov@gmail.com>
2018-10-25 13:05:47 +0500

    add drop_NA_col purpose

--- R/drop.R
+++ R/drop.R
@@ -12,7 +12,7 @@ drop_mono <- function(x){
   x[, apply(x, 2, function(x) length(unique(x)) > 1)]
 }
 
-
+# Delete columns that contain ONLY NAs
 drop_NA_col <- function(df){
     colz <- apply(is.na(df), 2, all)
     res <- df[ , !colz ]
