2b58f0351e111f857cc1c778ac55c13051703417
Vladimir Mikryukov <v.mikryukov@gmail.com>
2018-10-31 17:18:49 +0500

    roxygenize obj_load

--- R/obj_load.R
+++ R/obj_load.R
@@ -1,5 +1,16 @@
 
-# Load object from workspace
+#' Load object from workspace
+#'
+#' @param obj.name Name of the object that should be loaded from the workspace.
+#' @param wrksp.name File name containing the object of interest.
+#'
+#' @return Selected R-object.
+#'
+#' @examples
+#' x <- rnorm(100)
+#' save(x, file = "x.RData")
+#' obj_load("x", "x.RData")
+#' 
 obj_load <- function(obj.name = "x", wrksp.name = ".RData") {
   tmp.env <- new.env()                # create a temporary environment
   load(wrksp.name, envir = tmp.env)   # load workspace into temporary environment
