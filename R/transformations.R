284a14b8996a9ddd8da2c4482ec76a02f4b9b87f
Vladimir Mikryukov <v.mikryukov@gmail.com>
2018-11-07 17:56:29 +0500

    roxygenize IHS

--- R/transformations.R
+++ R/transformations.R
@@ -10,10 +10,23 @@ gelman_scale <- function(x){
   return(res)
 }
 
-# Inverse hyperbolic sine transformation
+
+#' Inverse hyperbolic sine transformation
+#'
+#' @description Inverse hyperbolic sine transformation. Unlike a log variable, the inverse hyperbolic sine is defined at zero.
+#' @param x Numeric vector
+#' @return Vector of transformed values.
+#' @details It is an alternative to log transformations when some of the variables take on zero or negative values and as an alternative to the Box-Cox when variables are zero or negative.
+#' Except for very small values of y, the inverse sine is approximately equal to log(2yi) or log(2)+log(yi), and so it can be interpreted in exactly the same way as a standard logarithmic dependent variable. For example, if the regression coefficient on "urban" is 0.1, that tells us that urbanites have approximately 10 percent higher wealth than non-urban people.
+#' @seealso \code{asinh}
+#' @references Burbidge J.B., Magee L., Robb A.L. Alternative transformations to handle extreme values of the dependent variable // Journal of the American Statistical Association. 1988. V. 83. № 401. P. 123-127.
+#' @examples
+#' IHS(seq(-10, 10, 1))
+#'
 IHS <- function(x){ 
   log(x + sqrt(x^2 + 1))
 }
+
 
 scale_to_interval <- function(x, limitMin = 0, limitMax = 1){
   res <- (limitMax - limitMin) * (x - min(x))
